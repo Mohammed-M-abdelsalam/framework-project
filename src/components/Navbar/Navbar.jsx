a85d5972fe0134904e035bac553d814a47a7c49f
Mohamed <mo.magdyabdelsalam@gmail.com>
2025-06-26 20:03:16 +0300

    change links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar(){
     return(
         <nav ref={navRef} className="fixed top-0 w-full p-3 transition-[padding] duration-600 z-100 bg-diffrent px-5">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Link to="framework-project" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">Start Framework</span>
                 </Link>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -29,13 +29,13 @@ function Navbar(){
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium flex flex-col rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse ">
                     <li>
-                        <NavLink to="about" className="block py-2 px-3 my-3 w-fit text-white transition-bg duration-500" aria-current="page">About</NavLink>
+                        <NavLink to="framework-project/about" className="block py-2 px-3 my-3 w-fit text-white transition-bg duration-500" aria-current="page">About</NavLink>
                     </li>
                     <li>
-                        <NavLink to="profile" className="block py-2 px-3 my-3 w-fit text-white transition-bg duration-500" aria-current="page">Profile</NavLink>
+                        <NavLink to="framework-project/profile" className="block py-2 px-3 my-3 w-fit text-white transition-bg duration-500" aria-current="page">Profile</NavLink>
                     </li>
                     <li>
-                        <NavLink to="contact" className="block py-2 px-3 my-3 w-fit text-white transition-bg duration-500" aria-current="page">Contact</NavLink>
+                        <NavLink to="framework-project/contact" className="block py-2 px-3 my-3 w-fit text-white transition-bg duration-500" aria-current="page">Contact</NavLink>
                     </li>
                 </ul>
                 </div>
